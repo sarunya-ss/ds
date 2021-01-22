49fc89e9c9cff1cba396bf338fee8b325c333e27
sarunya-ss <77414063+sarunya-ss@users.noreply.github.com>
2021-01-21 20:40:07 -0600

    Add files via upload

---
+++ R/ds.R
@@ -0,0 +1,16 @@
+#' Create Histrogam Boxplot and summary
+#' @export
+#' @param x numeric variable
+ds <- function(x) {
+  par(mfrow=c(1,2))
+  #Histrogram
+  his(x)
+  #Box plot
+  boxplot(x)
+  par(mfrow = c(1,1))
+  # Numeric summary
+  data.frame(min = min(x),
+             median = median(x),
+             mean = mean(x),
+             max = max(x))
+}
